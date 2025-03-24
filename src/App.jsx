58fe17b896918e54dade05323239e1fe04c6159d
regaruzianto <regaruzianto@gmail.com>
2025-03-24 21:26:25 +0700

    halaman utama

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
+import Produk from "./components/Produk/Produk";
 
 function App() {
 
   return (
-    <>
-
-      <h1 class="text-3xl font-bold italic">Hello world!</h1>
-    </>
+    <div>
+      <Produk />
+    </div>
   );
 }
 
---
+++ src/components/review/ReviewPreview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReviewPreview() {
+  return (
+    <div>ReviewPreview</div>
+  )
+}
+
+export default ReviewPreview
